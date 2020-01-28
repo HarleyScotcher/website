9f2e9c3b3188603c1853a647a6256f8d794be788
Harley Scotcher <harley@scotcher.me>
2020-01-28 08:05:12 +0000

    Initial commit

---
+++ config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteTitle: 'Harley Scotcher', // <title>
+  manifestName: 'Resume',
+  manifestShortName: 'Landing', // max 12 characters
+  manifestStartUrl: '/',
+  manifestBackgroundColor: '#663399',
+  manifestThemeColor: '#663399',
+  manifestDisplay: 'standalone',
+  manifestIcon: 'src/assets/img/website-icon.png',
+  pathPrefix: `/resume/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Harley',
+  lastName: ' Scotcher',
+  // social
+  socialLinks: [{
+      icon: 'fa-github',
+      name: 'Github',
+      url: 'https://github.com/HarleyScotcher',
+    },
+    {
+      icon: 'fa-linkedin-in',
+      name: 'Linkedin',
+      url: 'https://www.linkedin.com/in/harley-scotcher/',
+    },
+    {
+      icon: 'fas fa-file-alt',
+      name: 'Ciriculum Vitae',
+      url: 'https://www.google.com',
+    }
+  ],
+  email: 'harley@scotcher.me',
+  phone: '+447939088316',
+  address: 'Taunton, UK',
+};
