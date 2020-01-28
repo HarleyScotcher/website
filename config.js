e0aa1b1176514e6010418463e9ac1ece9fe1b5f5
Harley Scotcher <harley@scotcher.me>
2020-01-28 21:29:29 +0000

    Remove path prefix config value

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/resume/`, // This path is subpath of your hosting https://domain/portfolio
+  //pathPrefix: `/resume/`, // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Harley',
   lastName: ' Scotcher',
   // social
